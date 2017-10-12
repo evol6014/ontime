c1d8ad75d377a875f3ae962368e735c4cd56e871
java <java@bigdata>
2017-10-12 18:30:48 +0900

    mrunit add

--- src/main/java/com/example/dep/DepartureDelayCountJob.java
+++ src/main/java/com/example/dep/DepartureDelayCountJob.java
@@ -15,17 +15,26 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class DepartureDelayCountJob {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
+		
+//		conf.set("fs.defaultFS", "hdfs://bigdata01:8020");
+//		conf.set("yarn.resourcemanager.address", "bigdata01:8032");
+//		conf.set("mapreduce.framework.name", "yarn");
+//		conf.set("yarn.resourcemanager.scheduler.address", "bigdata01:8030");
 		
 		Job job = new Job(conf, "DepartureDelayCount");
 		
 		job.setJarByClass(DepartureDelayCountJob.class);
 		
-		FileInputFormat.setInputPaths(job, "/home/java/dataexpo/1987_nohead.csv");
-//		FileInputFormat.addInputPaths(job, "/home/java/dataexpo/1988_nohead.csv");
+		FileInputFormat.setInputPaths(job, "dataexpo/1987_nohead.csv");
+		FileInputFormat.addInputPaths(job, "dataexpo/1988_nohead.csv");
+//		FileInputFormat.addInputPaths(job, "dataexpo/1989_nohead.csv");
+//		FileInputFormat.addInputPaths(job, "dataexpo/1990_nohead.csv");
 		
-//		FileInputFormat.setInputPaths(job, "/home/java/dataexpo");
+//		FileInputFormat.addInputPaths(job, "dataexpo");
+		
 		job.setInputFormatClass(TextInputFormat.class);	// 생략가능
 		
 		job.setMapperClass(DepartureDelayCountMapper.class);
@@ -37,12 +46,15 @@ public class DepartureDelayCountJob {
 		job.setOutputValueClass(IntWritable.class);
 		
 		job.setOutputFormatClass(TextOutputFormat.class);// 생략가능
-		Path outputDir = new Path("/home/java/dataexpo_out/1987");
-//		Path outputDir = new Path("/home/java/dataexpo_out/total");
-		FileOutputFormat.setOutputPath(job, outputDir);
+		
+		String outputDir = "dataexpo_out/1988";
+//		String outputDir = "dataexpo_out/1990";
+//		String outputDir = "dataexpo_out/total";
+		
+		FileOutputFormat.setOutputPath(job, new Path(outputDir));
 		
 		FileSystem hdfs = FileSystem.get(conf);
-		hdfs.delete(outputDir, true);
+		hdfs.delete(new Path(outputDir), true);
 		
 		job.waitForCompletion(true);
 
